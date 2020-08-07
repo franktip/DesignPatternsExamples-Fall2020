71f2bc8aa1bd4513f1e01ddbf18ef15711ecd121
franktip <f.tip@northeastern.edu>
2020-08-07 10:56:05 -0400

    updated version of AbstractFactory (in folder abstractfactory2)

---
+++ src/abstractfactory2/createmaze.ts
@@ -0,0 +1,26 @@
+import Maze from './maze'
+import Room from './room'
+import Door from './door'
+import Direction from './direction';
+import Wall from './wall';
+import IMazeFactory from './imazefactory';
+
+function createMaze(factory: IMazeFactory): Maze {
+  var theMaze = factory.makeMaze();
+  var r1 = factory.makeRoom();
+  var r2 = factory.makeRoom();
+  var theDoor = factory.makeDoor(r1, r2);
+  theMaze.addRoom(r1);
+  theMaze.addRoom(r2);
+  r1.setSide(Direction.North, factory.makeWall());
+  r1.setSide(Direction.East, theDoor);
+  r1.setSide(Direction.South, factory.makeWall());
+  r1.setSide(Direction.West, factory.makeWall());
+  r2.setSide(Direction.North, factory.makeWall());
+  r2.setSide(Direction.East, factory.makeWall());
+  r2.setSide(Direction.South, factory.makeWall());
+  r2.setSide(Direction.West, theDoor);
+  return theMaze;
+}
+
+export default createMaze
---
+++ src/abstractfactory2/index.ts
@@ -0,0 +1,5 @@
+import createMaze from "./createmaze";
+import PlainMazeFactory from "./plainmazefactory";
+
+let factory = new PlainMazeFactory()
+createMaze(factory);
