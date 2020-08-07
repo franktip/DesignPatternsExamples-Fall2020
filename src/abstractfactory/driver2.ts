770075a6ab0fb9f038df16395c38079b7a1ad859
franktip <f.tip@northeastern.edu>
2020-08-07 13:07:35 -0400

    remove obsolete version of abstract factory

--- src/abstractfactory/driver2.ts
+++
@@ -1,7 +0,0 @@
-import MazeGame from "./mazegame";
-import MazeFactory from "./mazefactory"
-import EnchantedMazeFactory from "./enchantedmazefactory";
-
-var factory = new EnchantedMazeFactory();
-var game = new MazeGame();
-game.createMaze(factory);
