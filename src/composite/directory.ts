615a9c16ef6c72d226156c16ea8593fd861e3bc9
Mitchell Wand <wand@ccs.neu.edu>
2020-08-09 12:35:49 -0400

    added alternative coding in composite/directory.ts

--- src/composite/directory.ts
+++ src/composite/directory.ts
@@ -1,27 +1,51 @@
 import Node from './node'
 
 class Directory extends Node {
-  constructor(n: string, p?: Directory){ 
-    super(n,p);
-    this.children = []; 
+
+  private children: Array<Node>;
+
+  constructor(n: string, p?: Directory) {
+    super(n, p);
+    this.children = [];
   }
-  public getAbsoluteName() : string {
+
+  public getAbsoluteName(): string {
     return super.getAbsoluteName() + "/";
-  }  
-  public add(n: Node) : void {
-    this.children.push(n); 
   }
-  public find(s: string) : Array<string> {
+  public add(n: Node): void {
+    this.children.push(n);
+  }
+  public find(s: string): Array<string> {
     let result = new Array<string>();
-    if (this.name.indexOf(s) != -1){
+    if (this.name.indexOf(s) != -1) {
       result.push(this.getAbsoluteName());
     }
-    for (let child of this.children){
-      result.push(...child.find(s));   // TODO: Mitch to rewrite this more functionally?
-    }
-    return result;
+
+    // original
+    // for (let child of this.children) {     
+    //   result.push(...child.find(s));   // TODO: Mitch to rewrite this more functionally?
+    // }
+    // return result
+
+    // this one works
+    // foo.push(...) side-effects foo (but only returns the length of the result)
+    // this.children.forEach(function(child){result.push(...child.find(s))})
+    // return result;                                                        
+
+    // and this one works
+    // but foo.concat(...) is functional
+    // this.children.forEach(function (child) {
+    //   result = result.concat(child.find(s))
+    // })
+    // return result;
+
+    return this.children.reduce(function (sofar, child) {
+      return sofar.concat(child.find(s))
+    },
+      result)
   }
-  private children: Array<Node>;
 }
+
+
 
 export default Directory
